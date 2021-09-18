cae03a06c2ba0a1ed083b2a28711727c1276e203
Joseph Nicolas <josephnicolas10@gmail.com>
2021-09-18 17:10:40 -0400

    Added the basic template and finished create_grid

--- squares_game.py
+++ squares_game.py
@@ -1,9 +1,33 @@
-#first line
-#objective: create a memory squares game
+## To make it easier to start, make it text based
+## 0 is a white square
+## 1 is black square
+## have the rows and columns be constant
+## Note: Once you have accomplished these things, then you can change it
 
-# 1. Write out a hello world program
+## current steps
+## create a grid 
 
-def hello_world ():
-    print ("Hello world")
+class Squares_Game:
 
-hello_world()
+    def __init__ (self, row, col):
+        self.row = row
+        self.col = col
+    
+    ##Current step: Create a grid based on rows and columns
+    def create_grid (self):
+        print("the grid will have", self.row, "rows")
+        print("the grid will have", self.col, "columns")
+        for c in range(0, int(self.col)):
+            for r in range(0,int(self.row) - 1):
+                print("0", end='')
+            print("0")
+        
+# Eventually turn this into difficulties 
+# I.e. Easy, medium, and hard
+r = input("Number of rows? ")
+c = input("Number of columns ")
+3
+4
+g = Squares_Game(r,c)
+g.create_grid()
+
