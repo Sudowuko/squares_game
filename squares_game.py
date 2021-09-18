073a8e34b256254490f55fa42d8ea84cc9adcb48
Joseph Nicolas <josephnicolas10@gmail.com>
2021-09-18 18:10:28 -0400

    added a randomize function and some more bug fixes

--- squares_game.py
+++ squares_game.py
@@ -7,27 +7,52 @@
 ## current steps
 ## create a grid 
 
+from random import randrange
+
 class Squares_Game:
 
-    def __init__ (self, row, col):
-        self.row = row
-        self.col = col
+    def __init__ (self, row, col, lvl, grid):
+        self.row = int(row)
+        self.col = int(col)
+        self.lvl = int(lvl)
+        self.grid = grid
     
-    ##Current step: Create a grid based on rows and columns
+    ##Create_grid: Creates a grid based on user inputs for rows and columns
+    ##Errors I need to fix: Handle invalid data entries such as no integer inputs
     def create_grid (self):
         print("the grid will have", self.row, "rows")
         print("the grid will have", self.col, "columns")
-        for c in range(0, int(self.col)):
-            for r in range(0,int(self.row) - 1):
-                print("0", end='')
-            print("0")
+        self.grid = []
+        for c in range(self.row):
+            self.grid.append([])
+            for r in range(self.col):
+                self.grid[-1].append(0)
+                #print("0", end='')
+            #print("0")
+        #print(self.grid)
+            
+    ## Randomize_grid: Changes the grid values from 0 to 1 based on level
+    ## 1s represent the number you need to memorize for the game
+    def randomize_grid (self):
+        start_lvl = 0
+
+        #for i in range(int(self.lvl)):
+        while (start_lvl != self.lvl):
+            rand_x = randrange(self.row)
+            rand_y = randrange(self.col)
+            if ((self.grid[rand_x][rand_y]) == 0):
+                (self.grid[rand_x][rand_y]) = 1
+                start_lvl += 1
+
+        print("random grid")
+        print(self.grid)
         
 # Eventually turn this into difficulties 
 # I.e. Easy, medium, and hard
 r = input("Number of rows? ")
 c = input("Number of columns ")
-3
-4
-g = Squares_Game(r,c)
+
+g = Squares_Game(r, c, 9, [])
 g.create_grid()
+g.randomize_grid()
 
